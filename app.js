9b28397d73bc40ba814ec48aeb521fb191c57227
aleadr <91583325+aleadrkc@users.noreply.github.com>
2022-10-06 17:59:02 +0700

    Initial Deploy to Heroku

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
+app.get("/cikarang", (req, res) => { res.json({"lokasi": "cikarang"})});
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
